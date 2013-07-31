62882c401f68398a6d17c9b15c0e89c32edc8cc8
rafal.szemraj <rafael.szemraj@gmail.com>
2013-07-31 22:44:52 +0200

    added chart generation for node data

--- src/com/example/AirPollutionAndroidClient/AndroidNodeClientActivity.java
+++ src/com/example/AirPollutionAndroidClient/AndroidNodeClientActivity.java
@@ -1,7 +1,18 @@
 package com.example.AirPollutionAndroidClient;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.model.TimeSeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+import java.util.Iterator;
 
 public class AndroidNodeClientActivity extends Activity
 {
@@ -23,6 +34,7 @@ public class AndroidNodeClientActivity extends Activity
                    public void onComplete(Data data)
                    {
                        System.out.println(data);
+                       generateChart(data);
                    }
 
                    public void onError(Exception e)
@@ -31,4 +43,63 @@ public class AndroidNodeClientActivity extends Activity
                    }
                });
     }
+
+    public void generateChart(Data data)
+        {
+
+            XYMultipleSeriesRenderer renderer = new XYMultipleSeriesRenderer();
+            renderer.setApplyBackgroundColor(true);
+            renderer.setBackgroundColor(Color.BLACK);
+            renderer.setXLabelsColor(Color.YELLOW);
+            renderer.setShowGrid(true);
+            renderer.setYLabelsAlign(Paint.Align.LEFT);
+            renderer.setChartTitle("STATION DATA");
+            renderer.setXTitle("Time");
+            renderer.setYTitle("Pollution");
+            renderer.setShowLegend(false);
+            renderer.setZoomButtonsVisible(false);
+
+
+            int colors[] = { Color.WHITE, Color.YELLOW, Color.BLUE, Color.GREEN, Color.RED, Color.MAGENTA, Color.LTGRAY };
+
+
+
+            XYMultipleSeriesDataset mDataSet = new XYMultipleSeriesDataset();
+
+            Iterator<Data.DataEntry> dataEntryIterator = data.getData().iterator();
+            Data.DataEntry dataEntry;
+            TimeSeries series;
+            XYSeriesRenderer seriesRenderer;
+            while (dataEntryIterator.hasNext())
+            {
+
+                dataEntry = dataEntryIterator.next();
+                series = new TimeSeries(dataEntry.getMedium());
+                seriesRenderer = new XYSeriesRenderer();
+                int index = data.getData().indexOf( dataEntry );
+                index = ( index > ( data.getData().size() - 1 ) ) ? 0 : index;
+                seriesRenderer.setColor( colors[index] );
+
+                Iterator<Data.DataPoint> dataPointIterator = dataEntry.getData().iterator();
+                Data.DataPoint dataPoint;
+                while (dataPointIterator.hasNext())
+                {
+
+                    dataPoint = dataPointIterator.next();
+                    series.add(dataPoint.getDate(), dataPoint.getValue());
+
+                }
+
+                mDataSet.addSeries(series);
+                renderer.addSeriesRenderer( seriesRenderer );
+
+            }
+
+            GraphicalView mChart = ChartFactory.getTimeChartView(this, mDataSet, renderer, "HH");
+            FrameLayout chart = new FrameLayout(this);
+            chart.addView(mChart);
+            addContentView(chart, new FrameLayout.LayoutParams(FrameLayout.LayoutParams.FILL_PARENT, FrameLayout.LayoutParams.FILL_PARENT));
+
+
+        }
 }
